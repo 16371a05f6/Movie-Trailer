146437dbb7f96cac3150ab53444bb91522c4d7f3
keerthi <tadigirikeerthi156@gamil.com>
2019-01-09 16:02:56 +0530

    movie trailer

---
+++ media.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+class Tollywood():
+    def __init__(self,movie_title,movie_storyline,movie_poster_url,movie_youtube):
+        self.title=movie_title
+        self.storyline=movie_storyline
+        self.poster_image_url=movie_poster_url
+        self.trailer_youtube_url=movie_youtube
+    def show_movie_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
